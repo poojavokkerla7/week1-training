19da1ea704affc37397361b90afbb41de034f321
poojavokkerla7 <pooja.vokkerla@gen2soft.com>
2026-03-26 21:35:01 -0700

    Create valid_anagram.py

---
+++ dsa/hashmap/valid_anagram.py
@@ -0,0 +1,17 @@
+def is_anagram(s, t):
+    if len(s) != len(t):
+        return False
+
+    count = {}
+
+    for char in s:
+        count[char] = count.get(char, 0) + 1
+
+    for char in t:
+        if char not in count:
+            return False
+        count[char] -= 1
+
+    return True
+
+print(is_anagram("anagram", "nagaram"))
