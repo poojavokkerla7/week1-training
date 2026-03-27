3783788b318c601c85d511bd165f278a8f1261be
poojavokkerla7 <pooja.vokkerla@gen2soft.com>
2026-03-26 21:35:27 -0700

    Create subarray_sum_k.py

---
+++ dsa/hashmap/subarray_sum_k.py
@@ -0,0 +1,16 @@
+def subarray_sum(nums, k):
+    count = 0
+    prefix_sum = 0
+    hashmap = {0: 1}
+
+    for num in nums:
+        prefix_sum += num
+
+        if prefix_sum - k in hashmap:
+            count += hashmap[prefix_sum - k]
+
+        hashmap[prefix_sum] = hashmap.get(prefix_sum, 0) + 1
+
+    return count
+
+print(subarray_sum([1,1,1], 2))
